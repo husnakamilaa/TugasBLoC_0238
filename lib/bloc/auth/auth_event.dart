1f9d333a4098b867af84a5644644db3a5145b4c2
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-04-04 04:27:58 +0700

    feat: deklarasi login event, turunan dari authevent

--- lib/bloc/auth/auth_event.dart
+++ lib/bloc/auth/auth_event.dart
@@ -5,3 +5,13 @@ abstract class AuthEvent extends Equatable {
   List<Object?> get props => [];
 }
 
+class LoginSubmitted extends AuthEvent {
+  final String email;
+  final String password;
+
+  LoginSubmitted({required this.email, required this.password});
+
+  @override
+  List<Object?> get props => [email, password];
+}
+
