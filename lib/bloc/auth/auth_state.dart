dd5763edba3fb78b19f7b03da6a18682360d1723
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-04-04 04:28:25 +0700

    feat: deklarasi register event dari authevent

---
+++ lib/bloc/auth/auth_state.dart
@@ -0,0 +1,30 @@
+part of 'auth_bloc.dart';
+
+abstract class AuthState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class AuthInitial extends AuthState {}
+
+class AuthLoading extends AuthState {}
+
+class AuthAuthenticated extends AuthState {
+  final String email;
+  AuthAuthenticated({required this.email});
+
+  @override
+  List<Object?> get props => [email];
+}
+
+class AuthRegistered extends AuthState {}
+
+class AuthUnauthenticated extends AuthState {}
+
+class AuthError extends AuthState {
+  final String message;
+  AuthError({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
