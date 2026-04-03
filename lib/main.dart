92b1fe2188e0a324ff36000be9e2c15bd10c435c
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-04-04 00:33:14 +0700

    feat: copy main ui dari project navigasi sebelumnya

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:state_management/auth/login.dart';
+import 'package:state_management/mainui/home.dart';
+import 'package:state_management/page/order_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +33,12 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/home': (context) => const HomePage(),
+        '/order': (context) => const OrderPage(),
+      },
     );
   }
 }
